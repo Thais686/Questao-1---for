b3c8024891c01f3e91a8d3e1df2a7065eaf84bf7
Thaís <thaiscastro95@yahoo.com>
2024-09-25 17:22:41 -0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,16 @@
+let i
+
+let num= Number(prompt("Digite o numero da tabuada que voce quer saber "))
+
+
+
+for(i=0; i<=10 ;i++){
+
+
+    let mult= num*i
+
+console.log(`${num}X ${i}= ${mult}`)
+}
+
+
+
